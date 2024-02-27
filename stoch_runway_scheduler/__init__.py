e3d89fb6dc6ccea144246093b12c386bb1aff505
r-shone <36268080+r-shone@users.noreply.github.com>
2024-02-27 16:30:52 +0000

    Update __init__.py
    Added .norm

--- stoch_runway_scheduler/__init__.py
+++ stoch_runway_scheduler/__init__.py
@@ -1,5 +1,6 @@
 from .utils import weather, getcost, Normal_Conditional_GetServ, Normal_GetServ, Normal_GetServ_Future
 from .gamma import sample_cond_gamma, gamma_cond_exp, Gamma_GetServ, Gamma_GetServ_Future, Gamma_Conditional_GetServ, sample_gamma, gamma_create_cdf
+from .norm import norm_create_cdf
 from .annealing_cost import Annealing_Cost
 from .perm import Perm_Heur, Perm_Heur_New
 from .simulate import Calculate_FCFS, Posthoc_Check, Update_ETAs, Update_Stats, Serv_Completions
