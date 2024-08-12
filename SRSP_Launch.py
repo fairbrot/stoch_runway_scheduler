782e0b354c786c2eaa3fb69ee22e266fc00fafb4
Jamie Fairbrother <jfairbrother87@hotmail.com>
2024-08-12 10:36:54 +0100

    Moved separation parameters to config file

--- SRSP_Launch.py
+++ SRSP_Launch.py
@@ -22,24 +22,12 @@ DATA_DIR = '/home/jamie/Insync/fairbrot@lancaster.ac.uk/OneDrive Biz - Shared/SR
 
 # JF: should remove this when we can as NoA is set after reading data file
 NoA = 700 # number of aircraft - temporary value which will get changed later
-S = 40 # number of time slots (Rob not sure whether this is needed)
 
-w_rho = 10/9 # separation multiplier for bad weather (multiplies mean, not rate, so should be >1); we have used a reduction of 10% due to bad weather based on Odoni et al (2011) cited in Shone et al (2021)
 wiener_sig = 0.1 # standard deviation for Brownian motion
 weather_sig = wiener_sig # this assumption is being made in the paper for simplicity
 print('wiener_sig: '+str(wiener_sig))
 
 tau = 30 # Determines when an aircraft is deemed to have entered the pool. E.g. if tau=30, aircraft enters pool when its ETA is 30 minutes away from the current time.
-
-NoC = 4 # no. of aircraft classes
-# Time separations in seconds taken from Bennell et al (2017) with H, U, M, S as the 4 classes; 
-# the 5th array is for the situation where there is no leading aircraft
-Time_Sep = [[97,121,121,145],[72,72,72,97,97],[72,72,72,72],[72,72,72,72],[72,72,72,72]]
-# JF: Time_Sep is List[List[int]]
-
-# k controls variances of service times - larger means less variance
-# Chosen values correspond to specified values of coefficients of variation
-pot_k = [16, 25, 44, 100, 400] # Potential set of k values to sample from
 
 # lam1 and lam2 are the weights of scheduling delay and airborne holding delays - these are called theta^S and theta^W in the paper
 pot_lam1 = [0.1, 0.3, 0.5, 0.7, 0.9] # Potential set of values for lambda 1 to sample from
@@ -47,7 +35,6 @@ pot_lam1 = [0.1, 0.3, 0.5, 0.7, 0.9] # Potential set of values for lambda 1 to s
 # These correspond to gamma^S (thres1) and gamma^W (thres2) in paper
 pot_thres1 = [0, 15] # potential set of values of thres1 to sample from
 thres2 = 0
-
 
 # Min and max prescheduled time to consider in solution approach
 # Measured in minutes from from midnight? (6AM-2PM is when simulation runs between)
@@ -101,6 +88,9 @@ def main(cfg: DictConfig):
     flight_data, Alpha_Ps, Beta_Ps, late_means = read_flight_data(DATA_DIR + '/flight_pretac_data.csv',
                                                                 min_ps_time, max_ps_time, wiener_sig)
 
+    sep_cfg = cfg.separation
+    sep = ErlangSeparation(sep_cfg.time_sep, sep_cfg.k, sep_cfg.w_rho)
+
     #for rep in range(no_reps):
     # JF Question Why is for loop not used?: Rob not sure - may be fine to change back to for loop
     while rep < no_reps:
@@ -138,14 +128,6 @@ def main(cfg: DictConfig):
                     pax_weight[i] = 0.2*random.random() + 0.4 # Flights in the 'small' class have a passenger weight between 0.4 and 0.6
 
         SubPolicy = Policies[policy_index] # SubPolicy indicates the policy we are currently considering (e.g. SimHeur, DetHeur)
-
-        # Randomly generate the Erlang service time parameter
-
-        # Results can be stratified by k (roughlt 1 fifth of runs for each value of k)
-        k = random.choice(pot_k)
-        print('k: '+str(k))
-
-        sep = ErlangSeparation(Time_Sep, k, w_rho)
 
         # These are random for similar reasons pax_weight (g_i in paper) - results may be stratified by this as well
         # lam1 and lam2 are the weights of scheduling delay and airborne holding delays - these are called theta^S and theta^W in the paper
@@ -203,7 +185,7 @@ def main(cfg: DictConfig):
 
         # ArrTime_Sorted.sort(key=lambda x: x[0])
         # # Posthoc check validates that flight statistics are consistent with costs
-        # # posthoc_cost = Posthoc_Check(Left_queue, Ac_Info, ArrTime, ServTime, ArrTime_Sorted, weather_process, 0, NoA, w_rho, k, Time_Sep, cost_fn)
+        # # posthoc_cost = Posthoc_Check(Left_queue, Ac_Info, ArrTime, ServTime, ArrTime_Sorted, weather_process, 0, NoA, cfg.separation.w_rho, k, Time_Sep, cost_fn)
 
         # print('Done!')
 
@@ -220,9 +202,9 @@ def main(cfg: DictConfig):
             #     ServTime[i] = Ac_Info[i].service_rns
 
             # ArrTime_Sorted.sort(key=lambda x: x[0])
-            # FCFS_cost = Calculate_FCFS(Ac_Info, ArrTime, ServTime, ArrTime_Sorted, pool_max, list_min, weather_process, NoA, w_rho, k, Time_Sep, cost_fn)
-            # perm_heur_cost, AC_Used = Perm_Heur(Ac_Info, ArrTime, ServTime, ArrTime_Sorted, pool_max, list_min, weather_process, NoA, w_rho, k, Time_Sep, cost_fn, f1)
-            # perm_heur_cost, AC_Used = Perm_Heur_New(Ac_Info, ArrTime, ServTime, ArrTime_Sorted, pool_max, list_min, weather_process, NoA, w_rho, k, Time_Sep, cost_fn)
+            # FCFS_cost = Calculate_FCFS(Ac_Info, ArrTime, ServTime, ArrTime_Sorted, pool_max, list_min, weather_process, NoA, cfg.separation.w_rho, k, Time_Sep, cost_fn)
+            # perm_heur_cost, AC_Used = Perm_Heur(Ac_Info, ArrTime, ServTime, ArrTime_Sorted, pool_max, list_min, weather_process, NoA, cfg.separation.w_rho, k, Time_Sep, cost_fn, f1)
+            # perm_heur_cost, AC_Used = Perm_Heur_New(Ac_Info, ArrTime, ServTime, ArrTime_Sorted, pool_max, list_min, weather_process, NoA, cfg.separation.w_rho, k, Time_Sep, cost_fn)
             # policy_index=0
 
             rep += 1
